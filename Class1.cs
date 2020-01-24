9cd329601a0db2b4725d532e6b7ea80e7a1e145e
saisaketh2 <60268137+saisaketh2@users.noreply.github.com>
2020-01-24 12:13:40 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstConsoleCode
+{
+    class Class1
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Please provide your input1");
+            string userInput1 = Console.ReadLine();
+            Console.WriteLine("Please provide your input2");
+            string userInput2 = Console.ReadLine();
+            Console.WriteLine("Please provide your input3");
+            string userInput3 = Console.ReadLine();
+
+            int a = Convert.ToInt32(userInput1);
+            int b = Convert.ToInt32(userInput2);
+            int c = Convert.ToInt32(userInput3);
+
+            double s, h;
+            s = (a + b + c) / 2;
+            h = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            Console.WriteLine("Are using Herons formula " + h);
+        }
+            
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FirstConsoleCode
+{
+    class Program
+    {
+        private static object console;
+
+        static void Main(string[] args)
+
+        {
+            int x;
+            x = 3;
+            Console.WriteLine(x);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
